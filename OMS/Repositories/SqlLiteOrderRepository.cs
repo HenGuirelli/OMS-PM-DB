abb0446441af80531c9298140635779214b62de4
profGuirelli <hen.guirelli@gmail.com>
2023-05-31 13:59:43 -0300

    Update sql to use transactions

--- OMS/Repositories/SqlLiteOrderRepository.cs
+++ OMS/Repositories/SqlLiteOrderRepository.cs
@@ -2,36 +2,69 @@
 
 namespace OMS.Repositories
 {
-    public class SqlLiteOrderRepository : IOrderRepository
+    public class SqlLiteOrderRepository : IOrderRepository, IDisposable
     {
-        private string connectionString;
+        private readonly string connectionString;
+        private readonly SqliteConnection connection;
+        private readonly System.Timers.Timer _timer = new();
+        private SqliteTransaction? _transaction;
 
         public SqlLiteOrderRepository(string connectionString)
         {
             this.connectionString = connectionString;
+            connection = new SqliteConnection(connectionString);
+            connection.Open();
+
+            _transaction = connection.BeginTransaction();
+
+            _timer.Elapsed += (o, e) =>
+            {
+                var tx = _transaction;
+                _transaction = null;
+
+                tx.Commit();
+                _transaction = connection.BeginTransaction();
+            };
+            _timer.AutoReset = true;
+            _timer.Interval = 1000;
+            _timer.Start();
         }
 
         public void AddOrder(Order order)
         {
-            using (var connection = new SqliteConnection(connectionString))
+            using (var command = connection.CreateCommand())
             {
-                connection.Open();
+                command.CommandText = "INSERT INTO Orders (OrderId, Account, ClOrdId, Quantity, ExecutedQuantity, Price, Status, Symbol) " +
+                                      "VALUES (@OrderId, @Account, @ClOrdId, @Quantity, @ExecutedQuantity, @Price, @Status, @Symbol)";
+                command.Parameters.AddWithValue("@OrderId", order.OrderId);
+                command.Parameters.AddWithValue("@Account", order.Account);
+                command.Parameters.AddWithValue("@ClOrdId", order.ClOrdId);
+                command.Parameters.AddWithValue("@Quantity", order.Quantity);
+                command.Parameters.AddWithValue("@ExecutedQuantity", order.ExecutedQuantity);
+                command.Parameters.AddWithValue("@Price", order.Price);
+                command.Parameters.AddWithValue("@Status", order.Status);
+                command.Parameters.AddWithValue("@Symbol", order.Symbol);
 
-                using (var command = connection.CreateCommand())
-                {
-                    command.CommandText = "INSERT INTO Orders (OrderId, Account, ClOrdId, Quantity, ExecutedQuantity, Price, Status, Symbol) " +
-                                          "VALUES (@OrderId, @Account, @ClOrdId, @Quantity, @ExecutedQuantity, @Price, @Status, @Symbol)";
-                    command.Parameters.AddWithValue("@OrderId", order.OrderId);
-                    command.Parameters.AddWithValue("@Account", order.Account);
-                    command.Parameters.AddWithValue("@ClOrdId", order.ClOrdId);
-                    command.Parameters.AddWithValue("@Quantity", order.Quantity);
-                    command.Parameters.AddWithValue("@ExecutedQuantity", order.ExecutedQuantity);
-                    command.Parameters.AddWithValue("@Price", order.Price);
-                    command.Parameters.AddWithValue("@Status", order.Status);
-                    command.Parameters.AddWithValue("@Symbol", order.Symbol);
+                command.ExecuteNonQuery();
+            }
+        }
 
-                    command.ExecuteNonQuery();
-                }
+        public void UpdateOrder(Order order)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "UPDATE Orders SET Account = @Account, Quantity = @Quantity, " +
+                                      "ExecutedQuantity = @ExecutedQuantity, Price = @Price, " +
+                                      "Status = @Status, Symbol = @Symbol WHERE OrderId = @OrderId";
+                command.Parameters.AddWithValue("@Account", order.Account);
+                command.Parameters.AddWithValue("@Quantity", order.Quantity);
+                command.Parameters.AddWithValue("@ExecutedQuantity", order.ExecutedQuantity);
+                command.Parameters.AddWithValue("@Price", order.Price);
+                command.Parameters.AddWithValue("@Status", order.Status);
+                command.Parameters.AddWithValue("@Symbol", order.Symbol);
+                command.Parameters.AddWithValue("@OrderId", order.OrderId);
+
+                command.ExecuteNonQuery();
             }
         }
 
@@ -108,28 +141,9 @@ namespace OMS.Repositories
             return null;
         }
 
-        public void UpdateOrder(Order order)
+        public void Dispose()
         {
-            using (var connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-
-                using (var command = connection.CreateCommand())
-                {
-                    command.CommandText = "UPDATE Orders SET Account = @Account, Quantity = @Quantity, " +
-                                          "ExecutedQuantity = @ExecutedQuantity, Price = @Price, " +
-                                          "Status = @Status, Symbol = @Symbol WHERE OrderId = @OrderId";
-                    command.Parameters.AddWithValue("@Account", order.Account);
-                    command.Parameters.AddWithValue("@Quantity", order.Quantity);
-                    command.Parameters.AddWithValue("@ExecutedQuantity", order.ExecutedQuantity);
-                    command.Parameters.AddWithValue("@Price", order.Price);
-                    command.Parameters.AddWithValue("@Status", order.Status);
-                    command.Parameters.AddWithValue("@Symbol", order.Symbol);
-                    command.Parameters.AddWithValue("@OrderId", order.OrderId);
-
-                    command.ExecuteNonQuery();
-                }
-            }
+            connection.Dispose();
         }
     }
 
